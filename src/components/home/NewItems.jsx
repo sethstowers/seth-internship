d7d5ab7ef5da5354751f06b56fc27bb879c8b50c
Seth <stowers3099@gmail.com>
2024-08-13 16:48:45 -0500

    testing countdown

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
 
 const NewItems = () => {
+  const [newItemsData, setNewItemsData] = useState([]);
+
+  
+  async function getNewItems() {
+    const response = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems')
+    setNewItemsData(response.data)
+  }
+
+  const Timer = ({expDate}) => {
+    const [seconds, setSeconds] = useState()
+    let startTime;
+    let timeLeft = expDate - Date.now()
+    let millisElapsed;
+    let millisCountdown;
+
+    function start() {
+      startTime = Date.now()
+      requestAnimationFrame(updateCount);
+    }
+
+    function updateCount() {
+      requestAnimationFrame(updateCount);
+      millisElapsed = Date.now() - startTime;
+      millisCountdown = timeLeft - millisElapsed
+      let secondsLeft = Math.floor(millisCountdown / 1000)
+
+      setSeconds(secondsLeft)
+      console.log(seconds)
+    }
+
+    start()
+    return (
+      <div className="de_countdown">1</div>
+    )
+  }
+
+  useEffect(() => {
+    getNewItems()
+  }, [])
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -14,8 +55,8 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+          {newItemsData.map((item, index) => (
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={item.id}>
               <div className="nft__item">
                 <div className="author_list_pp">
                   <Link
@@ -24,12 +65,13 @@ const NewItems = () => {
                     data-bs-placement="top"
                     title="Creator: Monica Lucas"
                   >
-                    <img className="lazy" src={AuthorImage} alt="" />
+                    <img className="lazy" src={item.authorImage} alt="" />
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
-                <div className="de_countdown">5h 30m 32s</div>
-
+                <div>
+                  <Timer expDate={item.expiryDate} />
+                  </div>
                 <div className="nft__item_wrap">
                   <div className="nft__item_extra">
                     <div className="nft__item_buttons">
@@ -51,7 +93,7 @@ const NewItems = () => {
 
                   <Link to="/item-details">
                     <img
-                      src={nftImage}
+                      src={item.nftImage}
                       className="lazy nft__item_preview"
                       alt=""
                     />
@@ -59,12 +101,12 @@ const NewItems = () => {
                 </div>
                 <div className="nft__item_info">
                   <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{item.title}</h4>
                   </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
+                  <div className="nft__item_price">{item.price} ETH</div>
                   <div className="nft__item_like">
                     <i className="fa fa-heart"></i>
-                    <span>69</span>
+                    <span>{item.likes}</span>
                   </div>
                 </div>
               </div>
